f8fe8d0d38455293e3aad8ee1f0b1abe2208f6eb
1050440418@qq.com <1050440418@qq.com>
2018-04-11 11:03:27 +0800

    added

--- SSMlearn/src/main/java/my/lei/Main/TestMain.java
+++ SSMlearn/src/main/java/my/lei/Main/TestMain.java
@@ -1,4 +1,38 @@
 package my.lei.Main;
 
+import my.lei.mapper.UserMapper;
+import my.lei.pojp.User;
+import my.lei.utils.SQLSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
 public class TestMain {
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(TestMain.class);
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = SQLSessionFactoryUtils.openSqlsession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//            User user = userMapper.getUser("A0001");
+//            log.info(user.getUserName());
+//            User user=new User();
+//            user.setUserId("A0003");
+//            user.setUserName("娜美");
+//            user.setUserRemark("海贼王的女人");
+//            userMapper.deleteUser("A0003");
+//            sqlSession.commit();
+
+            List<User> users=userMapper.findUsers("lei");
+            for (int i = 0; i < users.size(); i++) {
+                System.out.println("name:" + users.get(i).getUserName());
+                System.out.println("remark:"+users.get(i).getUserRemark());
+            }
+        }finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
 }
--- SSMlearn/src/main/java/my/lei/mapper/UserMapper.java
+++ SSMlearn/src/main/java/my/lei/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package my.lei.mapper;
 
+import my.lei.pojp.User;
+
+import java.util.List;
+
 public interface UserMapper {
+    User getUser(String id);
+
+    int insertUser(User user);
+
+    int deleteUser(String userId);
+
+    int updateUser(User user);
+
+    List<User> findUsers(String userName);
 }
